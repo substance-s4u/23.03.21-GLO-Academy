24229fc8bbf53a7683e37482585b2c2788d2bdc4
Vitaliy <ps.substance@gmail.com>
2021-03-23 11:40:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,34 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+const scrollLink = document.querySelectorAll('a.scroll-link');
+
+(function() {
+	for (let i = 0; i < scrollLink.length; i++) {
+		scrollLink[i].addEventListener('click', function (event) {
+			event.preventDefault();
+			const id = scrollLink[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+		})
+	}
+})();
+
+const openModal = function () {
+	modalCart.classList.add('show');
+};
+
+const closeModal = function () {
+	modalCart.classList.remove('show');
+};
+
+
+
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
